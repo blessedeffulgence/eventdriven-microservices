2fa5e891a7db95bd67cdbaafdcc8c35bff5c53dc
onesmus Kahuhu <blessedeffulgence@gmail.com>
2022-06-14 15:07:12 +0300

    update for spring security in config server

---
+++ config-server/src/main/java/com/eventdriven/microservices/config/server/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.eventdriven.microservices.config.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class SecurityConfig  {
+
+
+    @Bean
+   SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+      return httpSecurity.antMatcher("/encrypt/**")
+               .antMatcher("/decrypt/**")
+               .build();
+   }
+}
